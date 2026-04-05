95ad2eaf22afa5a5383e332c53f6f4c17e4a8b4c
rayder2 <153458716+rayder2@users.noreply.github.com>
2026-04-05 15:21:12 -0500

    google

--- resources/views/layouts/app-public.blade.php
+++ resources/views/layouts/app-public.blade.php
@@ -2,9 +2,11 @@
 <html lang="es">
 
 <head>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inmobiliaria Mi Hogar</title>
+    google-site-verification: google10ea8e5eeda7ff88.html
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <style>
