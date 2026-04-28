82f7c33873a27fbf9724f5d305c6d666fbbf7686
rayder2 <153458716+rayder2@users.noreply.github.com>
2026-04-27 21:50:03 -0500

    actualizacion 63 tamaño imagen

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -9,7 +9,7 @@
     .hero-section {
         position: relative;
         width: 100%;
-        height: 100%;
+        height: 100vh;
         overflow: hidden;
         background: var(--color-black);
     }
