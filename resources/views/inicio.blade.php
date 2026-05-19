b9992a4f5b7633911ac3b3fd4b6719dccfafb929
rayder2 <153458716+rayder2@users.noreply.github.com>
2026-05-19 14:43:05 -0500

    actualizacion

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -252,14 +252,14 @@
 
         .t-foto {
             background: #ddd;
-            width: 100%;
-            height: 100%;
+            width: 70%;
+            height: 70%;
 
         }
 
         .t-foto img {
-            width: 100%;
-            height: 100%;
+            width: 70%;
+            height: 70%;
             display: block;
         }
 
