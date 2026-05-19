42861777da8b5bc1b6c7d03f97c7087e9d47adfa
rayder2 <153458716+rayder2@users.noreply.github.com>
2026-05-19 09:28:10 -0500

    actualizacion

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,8 +7,7 @@ php artisan config:clear
 echo "Ejecutando migraciones..."
 php artisan migrate --force || echo "Migraciones fallaron, continuando..."
 
-echo "Creando usuario admin..."
-php artisan db:seed --force || echo "Seeder falló, continuando..."
+
 
 echo "Iniciando Apache..."
 exec apache2-foreground
